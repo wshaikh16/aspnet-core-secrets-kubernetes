c5255498b225d7590988ec8b3d0131b7be32ddd3
wshaikh16 <shaikh_wahid@hotmail.com>
2020-11-19 22:30:45 -0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -25,9 +25,6 @@ namespace aspnet_core_secrets
                 .AddEnvironmentVariables();
 
             Configuration = builder.Build();
-
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
